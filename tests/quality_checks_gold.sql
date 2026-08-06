1c885acc79f6506a5f89cfb03e6b8a388427e7d6
ReehaKhan <62240108+ReehaKhan@users.noreply.github.com>
2026-08-06 21:06:36 +0500

    Update quality_checks_gold.sql
    Updated quality checks for Gold Layer to include checks for uniqueness of customer and product keys, as well as data model connectivity between fact and dimension tables.

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -1,24 +1,8 @@
 /*
-===============================================================================
-Quality Checks
-===============================================================================
-Script Purpose:
-    This script performs quality checks to validate the integrity, consistency, 
-    and accuracy of the Gold Layer. These checks ensure:
-    - Uniqueness of surrogate keys in dimension tables.
-    - Referential integrity between fact and dimension tables.
-    - Validation of relationships in the data model for analytical purposes.
-
-Usage Notes:
-    - Investigate and resolve any discrepancies found during the checks.
-===============================================================================
+Gold: Quality Checks
 */
 
--- ====================================================================
--- Checking 'gold.dim_customers'
--- ====================================================================
--- Check for Uniqueness of Customer Key in gold.dim_customers
--- Expectation: No results 
+-- gold.dim_customers
 SELECT 
     customer_key,
     COUNT(*) AS duplicate_count
@@ -26,11 +10,7 @@ FROM gold.dim_customers
 GROUP BY customer_key
 HAVING COUNT(*) > 1;
 
--- ====================================================================
--- Checking 'gold.product_key'
--- ====================================================================
--- Check for Uniqueness of Product Key in gold.dim_products
--- Expectation: No results 
+-- gold.product_key
 SELECT 
     product_key,
     COUNT(*) AS duplicate_count
@@ -38,10 +18,7 @@ FROM gold.dim_products
 GROUP BY product_key
 HAVING COUNT(*) > 1;
 
--- ====================================================================
--- Checking 'gold.fact_sales'
--- ====================================================================
--- Check the data model connectivity between fact and dimensions
+-- gold.fact_sales
 SELECT * 
 FROM gold.fact_sales f
 LEFT JOIN gold.dim_customers c
